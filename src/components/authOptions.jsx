63017a654961fc221ea43140f02666af020a0010
rey <njongremy30@gmail.com>
2025-10-06 15:43:47 +0100

    responsive on all devices

--- src/components/authOptions.jsx
+++ src/components/authOptions.jsx
@@ -7,13 +7,13 @@ const AuthOptions = () =>{
             <p className="font-roboto text-5xl mb-1.5 text-gray-600 text-center">Match<span className="text-[#62b1ff]">Point</span></p>
             <p className="text-center text-2xl text-gray-800 mb-5">Leveling up the comps Scene</p>
             <div className="flex flex-col justify-center w-full">
-                <button className="bg-[#62b1ff] text-white py-1.5 w-full h-[60px] my-2.5 rounded-lg text-2xl mb-5">Sign Up with Email</button>
+                <button className="bg-[#62b1ff] text-white py-1.5 w-full h-[60px] my-2.5 rounded-lg text-2xl mb-5 cursor-pointer">Sign Up with Email</button>
             <div className="flex w-full gap-4 mb-10">
-               <button className='flex h-[60px] shadow-2xl items-center justify-center rounded-lg w-[50%] text-2xl'>
-                 <img src={gIcon} className='w-7 h-7'/>
+               <button className='flex h-[60px] shadow-2xl items-center justify-center rounded-lg w-[50%] text-2xl cursor-pointer bg-white'>
+                 <img src={gIcon} className='w-7 h-7'/>&nbsp;
                  <span>Google</span>
                </button>
-               <button className='flex h-[60px] shadow-2xl items-center justify-center rounded-lg w-[50%] text-2xl bg-[#ee9e62] text-white'>
+               <button className='flex h-[60px] shadow-2xl items-center justify-center rounded-lg w-[50%] text-2xl bg-[#ee9e62] text-white cursor-pointer'>
                    <span>Sign In</span>
                </button>
             </div>
--- src/pages/authentication.jsx
+++ src/pages/authentication.jsx
@@ -4,8 +4,11 @@ import AuthOptions from '../components/authOptions';
 const Authentication = () =>{
 
    return(
-    <section className='w-full h-screen bg-cover bg-center' style={{backgroundImage: `url(${bg1})`}}>
-     <div className='flex items-baseline-last justify-center pb-6 w-full h-full bg-[linear-gradient(to_bottom,transparent,rgba(255,255,255,1))]'>
+    <section className='flex items-center justify-center w-full h-screen bg-cover bg-center sm:py-4' style={{backgroundImage: `url(${bg1})`}}>
+     <div className='flex items-baseline-last justify-center pb-6 w-full h-full bg-[linear-gradient(to_bottom,transparent,rgba(255,255,255,1))]
+                     sm:bg-none sm:w-[60%] sm:h-auto sm:bg-white/30 sm:backdrop-blur-md sm:border sm:border-white/20
+                     md:w-[50%]
+     '>
         <AuthOptions />
      </div>
     </section>
